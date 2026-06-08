57adeeff1fafe20061f6e053602b5c2e06bb9c37
Nikolai Popesku <nic1k@yandex.com>
2026-06-08 21:51:45 +0800

    fix: use books app label in rating migration

--- reading/migrations/0002_rating_scale_to_10.py
+++ reading/migrations/0002_rating_scale_to_10.py
@@ -4,7 +4,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("reading", "0001_initial"),
+        ("books", "0001_initial"),
     ]
 
     operations = [
