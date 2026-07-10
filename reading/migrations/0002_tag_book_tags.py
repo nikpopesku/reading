15a97c7448fb4e10cdb7e56769a1439122a5505f
nikpopesku <nic1k@yandex.com>
2026-07-10 14:02:15 +0200

    style: format legacy migration

--- reading/migrations/0002_tag_book_tags.py
+++ reading/migrations/0002_tag_book_tags.py
@@ -4,25 +4,29 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('books', '0001_initial'),
+        ("books", "0001_initial"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Tag',
+            name="Tag",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=100, unique=True)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+                    ),
+                ),
+                ("name", models.CharField(max_length=100, unique=True)),
             ],
             options={
-                'ordering': ['name'],
+                "ordering": ["name"],
             },
         ),
         migrations.AddField(
-            model_name='book',
-            name='tags',
-            field=models.ManyToManyField(blank=True, related_name='books', to='books.tag'),
+            model_name="book",
+            name="tags",
+            field=models.ManyToManyField(blank=True, related_name="books", to="books.tag"),
         ),
     ]
